db00b335974ea82dfba165586bf45f0e4556a303
David Kirk <dsak@bu.edu>
2019-10-22 14:36:48 -0400

    saving work - currently trying to get wifi to work

--- quest-3/code/wearable-firmware/main/wearable-firmware.c
+++ quest-3/code/wearable-firmware/main/wearable-firmware.c
@@ -3,10 +3,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+//rtos
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/event_groups.h"
+
+//gpio
 #include "driver/gpio.h"
+
+//networking
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "tcpip_adapter.h"
+//#include "protocol_examples_common.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
 
 /**
  * Brief:
@@ -88,6 +106,7 @@
  */
 
 
+//debouncer setup
 #define GPIO_OUTPUT_IO_0    18
 #define GPIO_OUTPUT_IO_1    19
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
@@ -95,21 +114,105 @@
 #define GPIO_INPUT_IO_1     5
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
 #define ESP_INTR_FLAG_DEFAULT 0
-#define MAX_DEBOUNCE_COUNT 40
+#define MAX_DEBOUNCE_COUNT 35
 #define DEBOUNCE_TIMEOUT 50
 
-int bounceCount; //number of bounces
-int startTime; //for recording elapsed time to determine if debouncer has timed out
-int interrupt_enabled = 1; //flag to disable interrupts during debouncing
+int bounceCount;                                        //number of bounces
+int startTime;                                          //for recording elapsed time to determine if debouncer has timed out
+int interrupt_enabled = 1;                              //flag to disable interrupts during debouncing
+int steps;                                              //number of steps taken by user (counter)
+static xQueueHandle gpio_evt_queue = NULL;              //event queue
 
-static xQueueHandle gpio_evt_queue = NULL;
+//flags
+int vibration_enabled;                                  //vibration sensor interrupt enable flag
+int thermistor_enabled;                                 //thermistor enable flag
+int battery_enabled;                                    //battery voltage enable flag
+int water_alarm_enabled;                                //water alert enable flag
 
+//timer setup
+int water_interval;                                     //time interval for the water alarm
+
+//socket setup
+#define HOST_IP_ADDR "192.168.1.101"                    //target server ip
+#define PORT 8080                                       //target server port
+char rx_buffer[128];
+char addr_str[128];
+int addr_family;
+int ip_protocol;
+int sock;                                               //socket id?
+struct sockaddr_in dest_addr;                           //socket destination info
+
+//wifi setup
+#define EXAMPLE_ESP_WIFI_SSID "Group_2"
+#define EXAMPLE_ESP_WIFI_PASS "smartkey"
+#define EXAMPLE_ESP_MAXIMUM_RETRY 10//CONFIG_ESP_MAXIMUM_RETRY
+
+static EventGroupHandle_t s_wifi_event_group;           //FreeRTOS event group to signal when we are connected
+const int WIFI_CONNECTED_BIT = BIT0;                    //The event group allows multiple bits for each event, but we only care about one event - are we connected to the AP with an IP?
+static const char *TAG = "wifi station";
+static int s_retry_num = 0;
+
+
+static void event_handler(void* arg, esp_event_base_t event_base, 
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:%s",
+                 ip4addr_ntoa(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+//connect to wifi
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    tcpip_adapter_init();
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+}
+
+//set up GPIO interrupts
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
+//GPIO interrupt task
 static void gpio_task_example(void* arg)
 {
     uint32_t io_num;
@@ -157,7 +260,8 @@ static void gpio_task_example(void* arg)
     }
 }
 
-static void gpio_init() {
+
+static void gpio_setup() {
 
     gpio_config_t io_conf;
     //disable interrupt
@@ -209,7 +313,7 @@ static void gpio_init() {
 void app_main(void)
 {
 
-    gpio_init();
+    gpio_setup();
 
     int cnt = 0;
     while(1) {
