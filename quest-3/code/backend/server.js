ec01612c88bf789e980ba98d01089e0ffb9bd49a
Kyle Martin <kylemartin@dhcp-wifi-8021x-155-41-87-32.bu.edu>
2019-10-23 17:24:17 -0400

    resolved post request issues

--- quest-3/code/backend/server.js
+++ quest-3/code/backend/server.js
@@ -1,10 +1,16 @@
 // Modules /////////////////////////////////////////////////////////////////////////////////////////////
 var express = require('express');
-var app = require('express')();
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var dgram = require('dgram');
 var bodyParser = require('body-parser');
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Port and IP
 var PORT = 3333; //internal is 1236, external is 3333
@@ -13,7 +19,11 @@ var HOST = "192.168.1.102"; //Kyle's Laptop is .102, Pi is .122
 // Create socket
 var server = dgram.createSocket('udp4');
 
+//file read
 const fs = require('fs');
+
+//Global state variable [vibration, temperature, battery, water, ping, time]
+var state = "111111800";
 
 //API ENDPOINTS/////////////////////////////////////////////////////////////////////////////////////////
 
@@ -31,30 +41,94 @@ app.get('/alldata', function(req, res){
   });
 });
 
-// Update device state
-app.post('/updatedevice', function(req, res){
-    var str = req.body.state;
-    server.send(str,remote.port,remote.address,function(error){
+// Update vibration state
+app.post('/vibration', jsonParser, function(req, res){
+    var num = req.body.state;
+    state = num.toString(10) + state.slice(1, 9);
+    server.send(state,remote.port,remote.address,function(error){
       if(error){
-        console.log('Found device: ' + str);
+        console.log('Found device');
       }
       else{
         console.log('Failed to update state');
       }
     });
+    res.send(state)
 });
 
-// Find device
-app.post('/finddevice', function(req, res){
-  var str = req.body.state;
-    server.send(str,remote.port,remote.address,function(error){
+// Update temperature state
+app.post('/temperature', jsonParser, function(req, res){
+    var num = req.body.state;
+    state = state[0] + num.toString(10) + state.slice(2, 9);
+    server.send(state,remote.port,remote.address,function(error){
       if(error){
-        console.log('Found device: ' + str);
+        console.log('Found device');
       }
       else{
-        console.log('Failed to find device');
+        console.log('Failed to update state');
       }
     });
+    res.send(state)
+});
+
+// Update battery state
+app.post('/battery', jsonParser, function(req, res){
+    var num = req.body.state;
+    state = state.slice(0, 2) + num.toString(10) + state.slice(3, 9);
+    server.send(state,remote.port,remote.address,function(error){
+      if(error){
+        console.log('Found device');
+      }
+      else{
+        console.log('Failed to update state');
+      }
+    });
+    res.send(state)
+});
+
+// Update water state
+app.post('/water', jsonParser, function(req, res){
+    var num = req.body.state;
+    state = state.slice(0, 3) + num.toString(10) + state.slice(4, 9);
+    server.send(state,remote.port,remote.address,function(error){
+      if(error){
+        console.log('Found device');
+      }
+      else{
+        console.log('Failed to update state');
+      }
+    });
+    res.send(state)
+});
+
+// Update ping state
+app.post('/ping', jsonParser, function(req, res){
+    var num = req.body.state;
+    state = state.slice(0,4) + num.toString(10) + state.slice(5, 9);
+    server.send(state,remote.port,remote.address,function(error){
+      if(error){
+        console.log('Found device');
+      }
+      else{
+        console.log('Failed to update state');
+      }
+    });
+    res.send(state)
+});
+
+// Update time interval
+app.post('/time', jsonParser, function(req, res){
+    var num = req.body.state;
+    state = state.slice(0,5) + num.toString(10);
+    server.send(state,remote.port,remote.address,function(error){
+      if(error){
+        console.log('Found device');
+      }
+      else{
+        console.log('Failed to update state');
+      }
+    });
+    res.send(state)
 });
 
 //SOCKET MESSAGE READER/////////////////////////////////////////////////////////////////////////////////
@@ -96,10 +170,6 @@ server.on('listening', function () {
 
 // Bind server to port and IP
 server.bind(PORT, HOST);
-
-// Link JSON parser to express
-app.use( bodyParser.json() ); 
-
 
 
 
