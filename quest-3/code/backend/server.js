88e4406ccf41d318159529fa83be3e343365f994
Kyle Martin <kylemartin@dhcp-wifi-8021x-155-41-87-32.bu.edu>
2019-10-23 20:42:08 -0400

    fixed frontend readoout

--- quest-3/code/backend/server.js
+++ quest-3/code/backend/server.js
@@ -12,9 +12,13 @@ var jsonParser = bodyParser.json();
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// Port and IP
+// Port and IP of Host
 var PORT = 3333; //internal is 1236, external is 3333
 var HOST = "192.168.1.102"; //Kyle's Laptop is .102, Pi is .122
+
+//Port and IP of Device
+var devPORT = 3333;
+var devHOST = "192.168.1.129"
 
 // Create socket
 var server = dgram.createSocket('udp4');
@@ -45,7 +49,7 @@ app.get('/alldata', function(req, res){
 app.post('/vibration', jsonParser, function(req, res){
     var num = req.body.state;
     state = num.toString(10) + state.slice(1, 9);
-    server.send(state,remote.port,remote.address,function(error){
+    server.send(state,devPORT,devHOST,function(error){
       if(error){
         console.log('Found device');
       }
@@ -60,7 +64,7 @@ app.post('/vibration', jsonParser, function(req, res){
 app.post('/temperature', jsonParser, function(req, res){
     var num = req.body.state;
     state = state[0] + num.toString(10) + state.slice(2, 9);
-    server.send(state,remote.port,remote.address,function(error){
+    server.send(state,devPORT,devHOST,function(error){
       if(error){
         console.log('Found device');
       }
@@ -75,7 +79,7 @@ app.post('/temperature', jsonParser, function(req, res){
 app.post('/battery', jsonParser, function(req, res){
     var num = req.body.state;
     state = state.slice(0, 2) + num.toString(10) + state.slice(3, 9);
-    server.send(state,remote.port,remote.address,function(error){
+    server.send(state,devPORT,devHOST,function(error){
       if(error){
         console.log('Found device');
       }
@@ -90,7 +94,7 @@ app.post('/battery', jsonParser, function(req, res){
 app.post('/water', jsonParser, function(req, res){
     var num = req.body.state;
     state = state.slice(0, 3) + num.toString(10) + state.slice(4, 9);
-    server.send(state,remote.port,remote.address,function(error){
+    server.send(state,devPORT,devHOST,function(error){
       if(error){
         console.log('Found device');
       }
@@ -105,7 +109,7 @@ app.post('/water', jsonParser, function(req, res){
 app.post('/ping', jsonParser, function(req, res){
     var num = req.body.state;
     state = state.slice(0,4) + num.toString(10) + state.slice(5, 9);
-    server.send(state,remote.port,remote.address,function(error){
+    server.send(state,devPORT,devHOST,function(error){
       if(error){
         console.log('Found device');
       }
@@ -120,7 +124,7 @@ app.post('/ping', jsonParser, function(req, res){
 app.post('/time', jsonParser, function(req, res){
     var num = req.body.state;
     state = state.slice(0,5) + num.toString(10);
-    server.send(state,remote.port,remote.address,function(error){
+    server.send(state,devPORT,devHOST,function(error){
       if(error){
         console.log('Found device');
       }
@@ -136,15 +140,9 @@ app.post('/time', jsonParser, function(req, res){
 // Send sensor readings to frontend
 server.on('message', function (message, remote) {
     io.emit('message', message);
-    // Send "Got data" acknowledgement
-    server.send("Got data",remote.port,remote.address,function(error){
-      if(error){
-        console.log('Did not send: Got data');
-      }
-      else{
-        console.log('Sent: Got data');
-      }
-    });
+    console.log(JSON.parse(message.toString()));
+    devPORT = remote.port;
+    devHOST = remote.address;
 });
 
 //HOST, SOCKET, AND EXPRESS INITIALIZATIONS/////////////////////////////////////////////////////////////
